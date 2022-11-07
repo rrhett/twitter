a7a1e5d3f7832c7dba77c96ad0ad0cf772782219
Rhett Robinson <rrhett@gmail.com>
2022-11-06 18:39:52 -0800

    Very early script that takes a list of user ids and adds them to a list

---
+++ follows_to_list.sh
@@ -0,0 +1,21 @@
+idsToAdd=()
+
+LIST=
+TOKEN=
+
+for i in ${!idsToAdd[@]}; do
+  start=0
+  end=$start+60
+  if [[ $i -ge $start && $i -lt $end ]]
+  then
+    echo "Going to add id ${idsToAdd[$i]}"
+    continue
+    curl \
+      -X POST \
+      -H "Content-type: application/json" \
+      -d "{\"user_id\":\"${idsToAdd[$i]}\"}" \
+      -H "Authorization: Bearer $TOKEN" \
+      "https://api.twitter.com/2/lists/${LIST}/members"
+    echo
+  fi
+done
